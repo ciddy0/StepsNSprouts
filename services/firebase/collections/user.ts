df14a9a69b8b526d71c427412002266dac03faf6
Sophia Doan <doansophia1@gmail.com>
2025-09-29 19:45:24 -0700

    implement user model

---
+++ services/firebase/collections/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: number;
+    age: number;
+    weight: number;
+    stepGoal: number;
+}
