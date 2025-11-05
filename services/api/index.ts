2feaa6a692fc47a315d7439580fcecc927774c00
Edwyn <119594108+edwyn-estrada@users.noreply.github.com>
2025-11-04 17:16:43 -0800

    add export lootService via API index barrel

---
+++ services/api/index.ts
@@ -0,0 +1,7 @@
+
+// Barrel file to export all API services in one place.
+
+export * from "./achievementService"; // if you have this
+export * from "./lootService"; // NEW service you just added
+export * from "./userService";
+
