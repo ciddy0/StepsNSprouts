996810d04323361746a82a5815a0483ed2073b39
ciddy0 <96404041+ciddy0@users.noreply.github.com>
2025-12-07 22:38:28 -0800

    STYLE: lowercase shop title

--- app/(tabs)/shop.tsx
+++ app/(tabs)/shop.tsx
@@ -170,7 +170,7 @@ export default function ShopScreen() {
                   fontSize: 28,
                   color: '#623B2A',
                 }}>
-                  SHOP
+                  shop
                 </Text>
               </ImageBackground>
 
